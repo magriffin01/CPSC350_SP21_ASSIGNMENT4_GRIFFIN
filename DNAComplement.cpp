35891f262ae80c5488e73765f0a435a28ea9b724
Mark Griffin <magriffin@chapman.edu>
2021-03-15 00:27:19 +0000

    Added DNAComplement.h and .cpp files

---
+++ DNAComplement.cpp
@@ -0,0 +1,2 @@
+#include "DNAComplement.h"
+
---
+++ DNAComplement.h
@@ -0,0 +1,16 @@
+#ifndef DNACOMPLEMENT_H
+#define DNACOMPLEMENT_H
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "GenStack.h"
+
+using namespace std;
+
+class DNAComplement
+{
+
+};
+
+#endif
